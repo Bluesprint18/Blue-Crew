9b80bedbedbf7506c9d2f9c91e85b2b5f3c46641
Pettar <64172282+pattar113@users.noreply.github.com>
2020-05-07 10:18:29 -0700

    Update HelloTeam.java

--- src/Sprint_01/HelloTeam.java
+++ src/Sprint_01/HelloTeam.java
@@ -7,6 +7,7 @@ public class HelloTeam {
         // Good job!!!
         // Yess we have our own repository now!!!
         // Hi team, that's great !!i'm  just practicing how to git push and git pull dont mind !!!
+        // still trying
 
     }
 }
