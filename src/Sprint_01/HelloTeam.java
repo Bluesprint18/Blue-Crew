9f990161407f7089cc6453d593cbe02ee6f5163e
vmargishvili <64220403+vmargishvili@users.noreply.github.com>
2020-05-08 16:01:41 -0700

    Update HelloTeam.java

--- src/Sprint_01/HelloTeam.java
+++ src/Sprint_01/HelloTeam.java
@@ -9,6 +9,7 @@ public class HelloTeam {
         // Hi team, that's great !!i'm  just practicing how to git push and git pull dont mind !!!
         // still trying
         // Hello Bluesprint18
+        // Hello my magic people! My try to commit. Have a warmest weekend. Val
 
     }
 }
