d5a204501ac17f40558559b24cb028422cf73962
smahma100 <smahma100@gmail.com>
2020-05-11 12:21:42 -0700

    add Sprint_01.java

--- src/Sprint_01/Rating.java
+++ src/Sprint_01/Rating.java
@@ -7,13 +7,13 @@ public class Rating {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Thank you for your order, your feadback is very important to us, would you please rate your experioence?");
+        System.out.println("Thank you for your order, your your feedback is greatly appreciated, would you please rate your experience?");
         String answear = scan.nextLine();
 
         if (answear.equalsIgnoreCase("yes") || answear.equalsIgnoreCase("sure") ||answear.equalsIgnoreCase("yeah")|| answear.equalsIgnoreCase("of course") || answear.equalsIgnoreCase("why not")){
-            System.out.println("From scale of 0 to 5; 5 being the best what you give?");
+            System.out.println("in a scale of 0 to 5 how would you rate your experience?");
             int rating = scan.nextInt();
-
+// under construction
         }
     }
 }
