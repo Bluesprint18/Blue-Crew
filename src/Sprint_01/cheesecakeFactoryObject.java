55d7f4445086c58132465fb3598454dab8006dd1
Zhaksybek <zhaks18121993@gmail.com>
2020-05-11 19:41:01 -0700

    created Object

--- src/Sprint_01/cheesecakeFactoryObject.java
+++ src/Sprint_01/cheesecakeFactoryObject.java
@@ -1,10 +1,25 @@
 package Sprint_01;
 
+import java.util.Scanner;
+
 public class cheesecakeFactoryObject {
 
     public static void main(String[] args) {
-
         // Here wi'll create our project
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("           *** Welcome to Cheesecake Factory App! ***\n" +
+                "\n  We have been serving our community for over 100 years. We have celebrated  Birthdays,\n" +
+                "Anniversaries and many other wonderful events with you delivering tasty and healthy food\n" +
+                "recipes of we inherited from our great grandparents.");
+
+
+
+
+
+
+
 
 
 
