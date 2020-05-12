dce828e92eec139fc83613c8783cca244a1325e2
pattar113 <pattar.dilm@gmail.com>
2020-05-11 23:23:16 -0700

    gg

--- src/Sprint_01/Rating.java
+++ src/Sprint_01/Rating.java
@@ -13,7 +13,7 @@ public class Rating {
         if (answear.equalsIgnoreCase("yes") || answear.equalsIgnoreCase("sure") ||answear.equalsIgnoreCase("yeah")|| answear.equalsIgnoreCase("of course") || answear.equalsIgnoreCase("why not")){
             System.out.println("in a scale of 0 to 5 how would you rate your experience?");
             int rating = scan.nextInt();
-// under construction
+
         }
     }
 }
