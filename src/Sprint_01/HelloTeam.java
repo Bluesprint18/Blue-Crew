bef38d2a72d59a555ce930161f98b57c0fbb1421
mr.jackson02 <62046646+Zhaksybek@users.noreply.github.com>
2020-05-12 18:39:18 -0700

    Update HelloTeam.java

--- src/Sprint_01/HelloTeam.java
+++ src/Sprint_01/HelloTeam.java
@@ -4,6 +4,7 @@ public class HelloTeam {
     public static void main(String[] args) {
 
         System.out.println("Hello Team This is our First Sprint");
+        // Test
         // Good job!!!
         // Yes we have our own repository now!!!
         // Hi team, that's great !!i'm  just practicing how to git push and git pull dont mind !!!
